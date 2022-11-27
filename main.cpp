32fcdc06062ff530d0cae298e109513e6838225e
d.romanichenko <d.romanichenko@epicentrk.ua>
2022-11-27 11:50:55 +0200

    stable and refactored algo

--- main.cpp
+++ main.cpp
@@ -3,27 +3,64 @@
 #include <set>
 #include <memory>
 
+void parseUniqWord(const std::string_view& target, std::set<std::string>* occurrence, std::string& prevPartStorage, size_t bytesRead)
+{
+	// parsing unique words
+	for (auto it = target.cbegin(); it != target.cend(); ++it)
+	{
+		// skipping whitespaces
+		for (; it != target.cend() && isspace(*it); ++it);
+
+		// if part contain only spaces, so nothing to store at this time, moving to the next round
+		if (it == target.cend())
+		{
+			break;
+		}
+
+		// measuring word length
+		size_t wordLen = 0;
+		auto wordStart = it;
+		for (; it != target.cend() && isalpha(*it); ++wordLen, ++it);
+
+		if (it == target.cend()) // if text block reach end, word is unfinished, so we need to backup this part
+		{
+
+			if (bytesRead == 0) // in case we reach end file last word counting as separate word
+			{
+				occurrence->insert(std::string(wordStart, wordLen));
+			}
+			else // reaching end of text block, backup will be stored
+			{
+				prevPartStorage = std::string(wordStart, wordLen);
+			}
+			break;
+		}
+		else // we found space, so add word to storage
+		{
+			occurrence->insert(std::string(wordStart, wordLen));
+		}
+	}
+}
+
 int main()
 {
+	constexpr size_t bufferSize = 16000; //16kb
 	auto occurrence = new std::set<std::string>(); //TODO: it can be just char* ?
 //TODO: if empty insert
 
 	std::ifstream file(R"(D:\Desktop\SubStrA\bigfile.txt)", std::ios::app | std::ios::ate);
-	//DEBUG: file size
-//	std::cout << file.tellg() << "Of Total Bytes" << "\n";
 	file.seekg(0, std::ios::beg); // rewind to the beginning of file
 
-	std::string      prevPart;
-	constexpr size_t bufferSize = 16000; //16kb
-
+	// portion reading
+	std::string prevPart;
 	while (file)
 	{
 		//reserve space for prev word part + new buffer size to concat word parts
-		size_t                  prevLength = prevPart.length();
-		std::unique_ptr<char[]> buffer     = std::make_unique<char[]>(prevLength + bufferSize);
+		size_t prevLength = prevPart.length();
+		std::unique_ptr<char[]> buffer = std::make_unique<char[]>(prevLength + bufferSize);
 
-		//copy prev part to buffer
-		if (prevLength != 0)
+		// restoring remaining word part to buffer
+		if (prevPart.empty())
 		{
 			for (int i = 0; i < prevLength; ++i)
 			{
@@ -36,57 +73,14 @@ int main()
 		file.read(buffer.get() + prevLength, bufferSize);
 		auto bytesRead = file.gcount();
 
-
 		//if file end we stop reading algo
 		if (bytesRead == 0 && prevLength == 0)
 		{
-			//TODO: save last word part to store
 			break; // EOF
 		}
 
-		//parsing unique words
-		const std::string_view& target = std::string_view(buffer.get(), bytesRead + prevLength);
-		//DEBUG: output buffer
-//		std::cout << "---buffer: " << target << std::endl;
-		auto it = target.begin();
-		int i = 0;
-		for (; it != target.end(); ++it, ++i)
-		{
-			//skipping whitespaces
-			for (; it != target.end() && isspace(*it); ++it, ++i);
-
-			//if part contain only spaces, so nothing to store at this time, moving to the next round
-			if (it == target.end())
-			{
-				break;
-			}
-
-			//measuring word length
-			int wordLen = 0;
-			for (; it != target.end() && std::isalpha(*it); ++wordLen, ++it);
-
-			//in case reaching the end, word remain is unfinished, we need to backup this part
-			if (it == target.end())
-			{
-				if (bytesRead == 0)
-				{
-					auto word = std::string(target, i, wordLen);
-					occurrence->insert(word);
-				}
-				else
-				{
-					prevPart = std::string(target, i, wordLen);
-				}
-				break;
-			}
-			else // or just add this to unique word storage
-			{
-				//unique filtering
-				auto word = std::string(target, i, wordLen);
-				occurrence->insert(word);
-				i += wordLen;
-			}
-		}
+		const std::string_view& target = std::string_view(buffer.get(), prevLength + bytesRead);
+		parseUniqWord(target, occurrence, prevPart, bytesRead);
 	}
 
 	//DEBUG: output buffer
