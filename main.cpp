dbb365577615feafd5c3f4f1ff1f67019c516a60
d.romanichenko <d.romanichenko@epicentrk.ua>
2022-11-27 12:12:49 +0200

    another refactoring

--- main.cpp
+++ main.cpp
@@ -3,85 +3,91 @@
 #include <set>
 #include <memory>
 
-void parseUniqWord(const std::string_view& target, std::set<std::string>* occurrence, std::string& prevPartStorage, size_t bytesRead)
+std::unique_ptr<char[]> InitializeBuffer(std::string& initWith, size_t len, const size_t bufferSize)
 {
-	// parsing unique words
-	for (auto it = target.cbegin(); it != target.cend(); ++it)
-	{
-		// skipping whitespaces
-		for (; it != target.cend() && isspace(*it); ++it);
+	std::unique_ptr<char[]> buffer = std::make_unique<char[]>(len + bufferSize);
 
-		// if part contain only spaces, so nothing to store at this time, moving to the next round
-		if (it == target.cend())
+	if (initWith.empty())
+	{
+		for (int i = 0; i < len; ++i) // restoring remaining word part to buffer
+		{
+			buffer[i] = initWith[i];
+		}
+		initWith.clear();
+	}
+
+	return buffer;
+}
+
+void parseWords(const std::string_view& str, std::set<std::string>* uniqWords, std::string& prevPart, size_t bytesRead)
+{
+	for (auto it = str.cbegin(); it != str.cend(); ++it)
+	{
+		for (; it != str.cend() && isspace(*it); ++it); // skipping whitespaces
+
+		if (it == str.cend()) // if contain only spaces, so nothing to store then moving to the next round
 		{
 			break;
 		}
 
-		// measuring word length
 		size_t wordLen = 0;
 		auto wordStart = it;
-		for (; it != target.cend() && isalpha(*it); ++wordLen, ++it);
+		for (; it != str.cend() && isalpha(*it); ++wordLen, ++it); // measuring word length
 
-		if (it == target.cend()) // if text block reach end, word is unfinished, so we need to backup this part
+		if (it == str.cend()) // if text block reach end, word is unfinished, so we need to backup this part
 		{
 
 			if (bytesRead == 0) // in case we reach end file last word counting as separate word
 			{
-				occurrence->insert(std::string(wordStart, wordLen));
+				uniqWords->insert(std::string(wordStart, wordLen));
 			}
 			else // reaching end of text block, backup will be stored
 			{
-				prevPartStorage = std::string(wordStart, wordLen);
+				prevPart = std::string(wordStart, wordLen);
 			}
 			break;
 		}
 		else // we found space, so add word to storage
 		{
-			occurrence->insert(std::string(wordStart, wordLen));
+			uniqWords->insert(std::string(wordStart, wordLen));
 		}
 	}
 }
 
-int main()
+std::set<std::string>* ParseUniqWordsFromFile(std::ifstream& file)
 {
 	constexpr size_t bufferSize = 16000; //16kb
-	auto occurrence = new std::set<std::string>(); //TODO: it can be just char* ?
-//TODO: if empty insert
-
-	std::ifstream file(R"(D:\Desktop\SubStrA\bigfile.txt)", std::ios::app | std::ios::ate);
 	file.seekg(0, std::ios::beg); // rewind to the beginning of file
-
-	// portion reading
+	auto occurrence = new std::set<std::string>(); //TODO: check it can be just char* ?
 	std::string prevPart;
+
 	while (file)
 	{
-		//reserve space for prev word part + new buffer size to concat word parts
 		size_t prevLength = prevPart.length();
-		std::unique_ptr<char[]> buffer = std::make_unique<char[]>(prevLength + bufferSize);
+		std::unique_ptr<char[]> buffer = InitializeBuffer(prevPart, prevLength, bufferSize);
 
-		// restoring remaining word part to buffer
-		if (prevPart.empty())
-		{
-			for (int i = 0; i < prevLength; ++i)
-			{
-				buffer[i] = prevPart[i];
-			}
-			prevPart.clear();
-		}
-
-		//reading new file part
-		file.read(buffer.get() + prevLength, bufferSize);
+		char *bufferStart = buffer.get() + prevLength;
+		file.read(bufferStart, bufferSize);
 		auto bytesRead = file.gcount();
 
-		//if file end we stop reading algo
-		if (bytesRead == 0 && prevLength == 0)
+		if (bytesRead == 0 && prevLength == 0) //if file end and prev parts too, we stop reading algo
 		{
-			break; // EOF
+			break;
 		}
 
 		const std::string_view& target = std::string_view(buffer.get(), prevLength + bytesRead);
-		parseUniqWord(target, occurrence, prevPart, bytesRead);
+		parseWords(target, occurrence, prevPart, bytesRead);
 	}
+
+	return occurrence;
+}
+
+int main()
+{
+//TODO: if empty insert
+
+	std::ifstream file(R"(D:\Desktop\SubStrA\bigfile.txt)", std::ios::app | std::ios::ate);
+	auto* occurrence = ParseUniqWordsFromFile(file);
 
 	//DEBUG: output buffer
 //	std::cout << "occurrence:" << std::endl;
